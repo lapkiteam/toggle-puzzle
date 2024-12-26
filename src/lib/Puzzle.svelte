c0af8ea88ab4063a9b9342c9123f2df08a338113
gretmn102 <erlgreywatson@gmail.com>
2024-12-27 01:08:07 +0200

    feat: display the puzzle

---
+++ src/lib/Puzzle.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Model } from "../model"
+
+  const lampsCount = 4
+  let puzzle = Model.create(lampsCount)
+</script>
+
+<div class="container">
+  {#each puzzle.lamps as lamp, index}
+    <button
+      class="lamp {lamp && "lamp-on"}"
+      on:click={() => {
+        puzzle = Model.toggle(puzzle, index)
+      }}
+    >
+      💡
+    </button>
+  {/each}
+</div>
+
+
+<style>
+  .container {
+    text-align: left;
+  }
+  .lamp {
+    transform-origin: center;
+  }
+  .lamp-on {
+    background-color: antiquewhite;
+  }
+</style>
