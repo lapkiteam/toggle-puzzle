dfa94f616abe2eac157ea56f673178616f73006d
gretmn102 <erlgreywatson@gmail.com>
2024-12-30 22:39:45 +0200

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-  import Puzzle from './lib/Puzzle.svelte'
+  import LinearPuzzle from "./lib/LinearPuzzle.svelte"
+  import CircularPuzzle from "./lib/CircularPuzzle.svelte"
 </script>
 
 <main>
   <h1>Puzzle</h1>
 
+  <h2>Linear</h2>
   <div class="card">
-    <Puzzle />
+    <LinearPuzzle />
+  </div>
+
+  <h2>Circular</h2>
+  <div class="card">
+    <CircularPuzzle />
   </div>
 </main>
 
---
+++ src/lib/CircularPuzzle.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { regularPolygon } from "../utils"
+  import { Model } from "../model"
+
+  const polygonRadius = 100
+  const lampsCount = 10
+  const lampCoords = regularPolygon(polygonRadius, polygonRadius, polygonRadius, 0, lampsCount)
+  let puzzle = Model.create(lampsCount)
+</script>
+
+<div class="container">
+  {#each puzzle.lamps as lamp, index}
+    <button
+      class="lamp {lamp && "lamp-on"}"
+      style:transform="translate(calc({lampCoords[index][0]}px - 50%), calc({lampCoords[index][1]}px - 50%))"
+      on:click={() => {
+        puzzle = Model.toggle(puzzle, index)
+      }}
+    >
+      💡
+    </button>
+  {/each}
+</div>
+
+
+<style>
+  .container {
+    width: 200px;
+    height: 200px;
+    /* background-color: red; */
+    /* border-radius: 100%; */
+
+    text-align: left;
+  }
+  .lamp {
+    position: absolute;
+    transform-origin: center;
+  }
+  .lamp-on {
+    background-color: antiquewhite;
+  }
+</style>
